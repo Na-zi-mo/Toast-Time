f27e37a53b7301ea33ca68128298a51a42f7da75
Na-zi-mo <nazimad2004@gmail.com>
2024-11-06 10:36:34 -0500

    Ennemy Movement Handled #7 #8 #9 @nbourre

---
+++ Toast Time/ennemies/candy apple zombie/candy_apple_zombie.gd
@@ -0,0 +1,34 @@
+class_name CandyAppleZombie
+extends GenericCharacter
+
+
+@export var GRAVITY = 10
+
+const ACCEL = 75.0
+
+var accel = ACCEL
+
+var facing_right : bool = true
+
+var current_sprite : Sprite2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	anim_player = $AnimationPlayer
+	current_sprite = $Sprite2D
+	anim_player.play("idle")
+	direction = Vector2(scale.x, 0)
+
+# Called every physics frame
+func _physics_process(delta: float) -> void:
+	handle_gravity(delta)
+	
+		
+	if (velocity.x > 0):
+		current_sprite.flip_h = false
+	elif (velocity.x < 0):
+		current_sprite.flip_h = true
+		
+func handle_gravity(delta : float):
+	if not is_on_floor():
+		velocity.y += GRAVITY * delta

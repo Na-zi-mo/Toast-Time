4b7e2825014cc72643f007e173c79144eb40f89f
Na-zi-mo <nazimad2004@gmail.com>
2024-11-06 14:30:43 -0500

    Refactored Code For Characters

--- Toast Time/characters/ennemies/marauder leader/marauder_leader.gd
+++ Toast Time/characters/ennemies/marauder leader/marauder_leader.gd
@@ -2,25 +2,26 @@ class_name MarauderLeader
 extends GenericCharacter
 
 @export var GRAVITY = 10
-
-var current_sprite : Sprite2D
+@export var MAXFALLSPEED = 200
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	anim_player = $AnimationPlayer
-	current_sprite = $Sprite2D
+	sprite = $Sprite2D
 	anim_player.play("idle")
 	direction = Vector2(scale.x, 0)
 
 # Called every physics frame
 func _physics_process(delta: float) -> void:
-	handle_gravity(delta)
 	
 	if (velocity.x > 0):
-		current_sprite.flip_h = false
+		sprite.flip_h = false
 	elif (velocity.x < 0):
-		current_sprite.flip_h = true
-		
-func handle_gravity(delta : float):
-	if not is_on_floor():
-		velocity.y += GRAVITY * delta
+		sprite.flip_h = true
+	
+	motion.y += GRAVITY
+	
+	if motion.y > MAXFALLSPEED:
+		motion.y = MAXFALLSPEED
+	velocity = motion
+	move_and_slide()

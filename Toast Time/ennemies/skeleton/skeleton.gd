83a6266793933fe2b4babe18a8f3fdccd3ee2cce
Na-zi-mo <nazimad2004@gmail.com>
2024-11-20 14:08:10 -0500

    Added New Enemy

---
+++ Toast Time/ennemies/skeleton/skeleton.gd
@@ -0,0 +1,32 @@
+class_name Skeleton
+extends GenericCharacter
+
+
+@export var GRAVITY = 10
+@export var MAXFALLSPEED = 200
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	anim_player = $AnimationPlayer
+	sprite = $Sprite2D
+	#anim_player.play("idle")
+	health_component = HealthClass.new()
+	health_component.health = 30
+	health_component.max_health = 30
+	direction = Vector2(scale.x, 0)
+
+# Called every physics frame
+func _physics_process(delta: float) -> void:
+	
+	if (velocity.x > 0):
+		sprite.flip_h = false
+	elif (velocity.x < 0):
+		sprite.flip_h = true
+		
+	motion.y += GRAVITY
+	
+	if motion.y > MAXFALLSPEED:
+		motion.y = MAXFALLSPEED
+	velocity = motion
+	move_and_slide()
+		

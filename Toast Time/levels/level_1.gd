0419ecf1b21410825b9fe4c1fbf5b64827465efa
Na-zi-mo <nazimad2004@gmail.com>
2024-11-20 14:30:30 -0500

    Added Pause To both levels

--- Toast Time/levels/level_1.gd
+++ Toast Time/levels/level_1.gd
@@ -1,10 +1,22 @@
 extends Node2D
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass
+@onready var pause_menu = $HUD/Pause
+var paused = false
+
+
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if Input.is_action_just_pressed("pause"):
+		pause_resume()
+
+func pause_resume():
+	if paused:
+		pause_menu.hide()
+		Engine.time_scale = 1
+	else:
+		pause_menu.show()
+		Engine.time_scale = 0
+	paused = !paused
--- Toast Time/levels/level_2.gd
+++ Toast Time/levels/level_2.gd
@@ -1,6 +1,25 @@
 extends Node2D
 
 
+@onready var pause_menu = $HUD/Pause
+var paused = false
+
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("pause"):
+		pause_resume()
+
+func pause_resume():
+	if paused:
+		pause_menu.hide()
+		Engine.time_scale = 1
+	else:
+		pause_menu.show()
+		Engine.time_scale = 0
+	paused = !paused
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,8 +29,3 @@ func _ready() -> void:
 	
 	hud.update_health(player.health_component.health, player.health_component.max_health)
 	
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass

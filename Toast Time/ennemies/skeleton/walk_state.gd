ad8f0a4f200f569af5232a7bf9935ca33a15a701
Na-zi-mo <nazimad2004@gmail.com>
2024-12-01 16:06:31 -0500

    Skeleton Accel fixed

--- Toast Time/ennemies/skeleton/walk_state.gd
+++ Toast Time/ennemies/skeleton/walk_state.gd
@@ -5,7 +5,8 @@ extends BaseState
 var player 
 var diff : float
 
-var direction
+var ACCEL = 50
+var dir
 
 
 func get_player() -> CharacterBody2D:
@@ -15,11 +16,12 @@ func enter() -> void:
 	player = get_player()
 	character.get_animation_player().play("walk")
 	
-	direction = -1 if (diff < 0) else 1
+	dir = -1 if (diff < 0) else 1
 
 func update(delta: float) -> void:
 	diff = (player.position.x - character.position.x) 
-	direction = -1 if (diff < 0) else 1 
+	dir = -1 if (diff < 0) else 1 
 
 func physics_update(delta: float) -> void:
-	character.motion.x += delta * direction * 50
+	
+	character.motion.x = dir * ACCEL

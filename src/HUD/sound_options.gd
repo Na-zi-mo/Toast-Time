ea9a3e424799730ce31ca784f026518a278d92ad
Na-zi-mo <nazimad2004@gmail.com>
2024-12-02 10:58:09 -0500

    Added Sfx and Master Volume

--- src/HUD/sound_options.gd
+++ src/HUD/sound_options.gd
@@ -1,9 +1,13 @@
 extends Control
 
-@onready var slider : HSlider = $VBoxContainer/HSlider
+@onready var music_slider : HSlider = $VBoxContainer/MusicSlider
+@onready var master_slider : HSlider = $VBoxContainer/MasterSlider
+@onready var sfx_slider : HSlider = $VBoxContainer/SfxSlider
 
 func _ready() -> void:
-	slider.value = db_to_linear(SoundManager.get_current_music().volume_db)
+	music_slider.value = db_to_linear(AudioServer.get_bus_volume_db(AudioServer.get_bus_index("Music")))
+	master_slider.value = db_to_linear(AudioServer.get_bus_volume_db(AudioServer.get_bus_index("Master")))
+	sfx_slider.value = db_to_linear(AudioServer.get_bus_volume_db(AudioServer.get_bus_index("SFX")))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -16,6 +20,16 @@ func _on_back_pressed() -> void:
 	get_tree().change_scene_to_file("res://HUD/options.tscn")
 
 
-func _on_h_slider_drag_ended(value_changed: bool) -> void:
-	var value = slider.value
-	SoundManager.get_current_music().volume_db = linear_to_db(value)
+func _on_music_slider_drag_ended(value_changed: bool) -> void:
+	var value = music_slider.value
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Music"), linear_to_db(value))
+
+
+func _on_master_slider_drag_ended(value_changed: bool) -> void:
+	var value = master_slider.value
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), linear_to_db(value))
+
+
+func _on_sfx_slider_drag_ended(value_changed: bool) -> void:
+	var value = sfx_slider.value
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("SFX"), linear_to_db(value))

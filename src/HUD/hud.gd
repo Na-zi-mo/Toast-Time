a114d2de970c648c9074d512938404b09b834bdc
Na-zi-mo <nazimad2004@gmail.com>
2024-12-02 22:16:16 -0500

    Added Shortcut for both ends

--- src/HUD/hud.gd
+++ src/HUD/hud.gd
@@ -1,6 +1,7 @@
 extends CanvasLayer
 class_name HUD
 
+const LEVEL_FILE_PATH = "res://levels/level_"
 
 @onready var player : Player
 @onready var health_bar : GenericProgressBar
@@ -40,3 +41,15 @@ func _input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("debug"):
 		debug_flag = !debug_flag
 		debug_label.visible = debug_flag
+		
+	if Input.is_action_just_pressed('suicide'):
+		player.take_damage(100)
+		
+	if Input.is_action_just_pressed('cheat'):
+		var current_scene_file = get_parent().scene_file_path
+		if current_scene_file.to_int() == 1:
+			var next_level_number = current_scene_file.to_int() + 1
+			var next_level_path = LEVEL_FILE_PATH + str(next_level_number) + ".tscn"
+			PlayerData.save(player)
+			get_tree().change_scene_to_file(next_level_path)
+		player.position = Vector2(2186,394)
--- src/levels/player_data.gd
+++ src/levels/player_data.gd
@@ -1,7 +1,7 @@
 extends Node
 
 
-# Called when the node enters the scene tree for the first time.
+
 var position: Vector2
 var health: int = 100
 var inventory: Array = []
@@ -10,10 +10,8 @@ var inventory: Array = []
 func save(player: Node2D) -> void:
 	#position = player.global_position
 	health = player.health_component.health  
-	#inventory = player.inventory 
 
 # Apply the saved state to a new player
 func apply(player: Node2D) -> void:
 	#player.global_position = position
 	player.health_component.health   = health
-	#player.inventory = inventory
